1ef3897e895bd2e89c2c6c4b8c4c39aa6f631f4b
Sediqullah Amiri <95490813+sediqamiri@users.noreply.github.com>
2022-10-06 14:35:47 +0430

    24;40

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,14 @@
 const navMenu = document.getElementById('nav-menu'),
     navToggle = document.getElementById('nav-toggle'),
     navClose = document.getElementById('nav-close')
+
+    /* Menu Show */
+    /* Validate if constant exists */
+
+    if (navToggle) {
+        navToggle.addEventListener('click', () =>{
+            navMenu.classList.add('show-menu')
+        })
+    }
+
+     
