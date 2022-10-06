4e781115dae1b0f5055940ce080efe6c2fac619e
Sediqullah Amiri <95490813+sediqamiri@users.noreply.github.com>
2022-10-06 14:30:23 +0430

    new

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+/* Menu Show Y Hidden */
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
